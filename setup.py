91f204a49fcf23f4f2cd4099a9ef1e36e67338de
Szymon Mikler <sjmikler@gmail.com>
2024-05-06 19:24:10 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,7 @@ long_description = package_relative_path("README.md").read_text(encoding="UTF-8"
 setup(
     name="playsound3",
     version="2.0.0-alpha",
-    description=long_description,
-    long_description=long_description,
+    description="Cross-platform library to play audio files",
     url="https://github.com/sjmikler/playsound3",
     author="Szymon Mikler",
     author_email="sjmikler@gmail.com",
@@ -37,4 +36,6 @@ setup(
     install_requires=[
         "pygobject; platform_system=='Linux'",
     ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
