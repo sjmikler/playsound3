3ebc110d922d907c1bd5ad2ec3e08258bb7c5b11
Szymon Mikler <sjmikler@gmail.com>
2024-05-07 01:58:19 +0200

    Restore python 3.7 compatibility

--- setup.py
+++ setup.py
@@ -22,6 +22,8 @@ setup(
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
