c129ed50db97e5edd01be4fd4f53215b1e7b272b
Szymon Mikler <sjmikler@gmail.com>
2025-03-18 02:15:42 +0100

    Minor test fix

--- tests/test_unit_errors.py
+++ tests/test_unit_errors.py
@@ -5,6 +5,8 @@ import pytest
 from playsound3 import playsound
 from playsound3.playsound3 import PlaysoundException
 
+valid = "tests/sounds/sample3s.mp3"
+
 
 def test_invalid_sound_file():
     with pytest.raises(PlaysoundException):
@@ -18,7 +20,7 @@ def test_non_existent_file():
 
 def test_invalid_backend():
     with pytest.raises(PlaysoundException):
-        playsound("valid.mp3", backend="invalid_backend")
+        playsound(valid, backend="invalid_backend")
 
 
 def test_playsound_from_url():
